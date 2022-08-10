2a1a378019bef4891e902a31ecc95fe3127ed546
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2022-08-10 20:51:10 -0300

    a

--- frontend/system-front/src/index.jsx
+++ frontend/system-front/src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
+import ClientRegister from "./clientRegister";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,6 +12,10 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/clientRegister" element={<ClientRegister />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

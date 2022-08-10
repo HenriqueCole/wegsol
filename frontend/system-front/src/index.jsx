578983b9ae42977a1f1e4edd2491edc5b4f396e5
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2022-08-10 19:35:34 -0300

    corerct

--- frontend/system-front/src/index.jsx
+++ frontend/system-front/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

a90c42da9aaaf0858c169d0b3a60e19d00b9c6e3
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2022-08-10 19:06:04 -0300

    separação do grupo

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB60rXqOs2WuO_Sa4rSbabAjh6kccwG2-Y",
+  authDomain: "tear-system.firebaseapp.com",
+  projectId: "tear-system",
+  storageBucket: "tear-system.appspot.com",
+  messagingSenderId: "167425157602",
+  appId: "1:167425157602:web:0d152536885b72c3bd31bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

c9fff13af7b09112027373761389a730ba710076
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2022-08-10 19:38:30 -0300

    route

--- frontend/system-front/src/index.jsx
+++ frontend/system-front/src/index.jsx
@@ -6,6 +6,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );

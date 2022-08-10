3dc025ffda59d91bb5c1ea6af1c9c86679567d8a
Kenzo Sato <kenzo_sato@estudante.sc.senai.br>
2022-08-10 19:33:50 -0300

    created organization

---
+++ backend/router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+/*Examples
+const clientes = require("./api//clientes/clientes.controller");
+router.use("/clientes", clientes);
+*/
+
+module.exports = router;

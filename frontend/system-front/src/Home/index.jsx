f4a4124f8c30533e5ac7ac121050a4969f6c135f
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-08-10 20:37:57 -0300

    :+1:

--- frontend/system-front/src/Home/index.jsx
+++ frontend/system-front/src/Home/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Sidebar from '../Components/sidebar'
+import './styles.scss'
+
 
 export default function App() {
   return (
     <div className="containerPageHome">
-      <header></header>
-      <main></main>
+      <div className="quadrado-cima"></div>
+      
+      <div className="quadrados">
+        <div className="quadrado-esquerda"></div>
+        <div className="quadrado-direita"></div>
+      </div>
+
     </div>
   );
 }

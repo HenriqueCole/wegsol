dd191d113b7c9111e27647bddcb85b252a9aedcc
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-08-10 20:45:29 -0300

    :+1:

--- frontend/system-front/src/Home/index.jsx
+++ frontend/system-front/src/Home/index.jsx
@@ -4,12 +4,16 @@ import './styles.scss'
 
 
 export default function App() {
+ 
+
   return (
     <div className="containerPageHome">
       <div className="quadrado-cima"></div>
-      
+
       <div className="quadrados">
-        <div className="quadrado-esquerda"></div>
+        <div className="quadrado-esquerda">
+         
+        </div>
         <div className="quadrado-direita"></div>
       </div>
 

1c318d9848518a4383ba941c1d012d01544ed27b
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-08-10 20:00:24 -0300

    pasta components

---
+++ frontend/system-front/src/Components/sidebar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+export default function App() {
+    return <>
+        <div className="entireContainer">
+            <div className="itenscontainer">
+                <div className="homeContainer">
+                    <div className="icon-home">
+                        <img src="./assets/casa.png" alt="" className="icon-home-main" />
+                    </div>
+                    <div className="text-home">
+                        <h1 className="text-home-main"></h1>
+                    </div>
+                </div>
+                <div className="registerClient">
+                    <div className="icon-registerClient">
+                        <img src="s" alt="" className="icon-registerClient-main" />
+                    </div>
+                    <div className="text-registerClient">
+                        <h1 className="text-registerClient-main"></h1>
+                    </div>
+                </div>
+                <div className="registerMachines">
+                    <div className="icon-registerMachines">
+                        <img src="s" alt="" className="icon-registerMachines-main" />
+                    </div>
+                    <div className="text-registerMachines">
+                        <h1 className="text-registerMachines-main"></h1>
+                    </div>
+                </div>
+                <div className="registerProduct">
+                    <div className="icon-registerProduct">
+                        <img src="s" alt="" className="icon-registerProduct-main" />
+                    </div>
+                    <div className="text-registerProduct">
+                        <h1 className="text-registerProduct-main"></h1>
+                    </div>
+                </div>
+                <div className="registerProvider">
+                    <div className="icon-registerProvider">
+                        <img src="s" alt="" className="icon-registerProvider-main" />
+                    </div>
+                    <div className="text-registerProvider">
+                        <h1 className="text-registerProvider-main"></h1>
+                    </div>
+                </div>
+                <div className="registerMesh">
+                    <div className="icon-registerMesh">
+                        <img src="s" alt="" className="icon-registerMesh-main" />
+                    </div>
+                    <div className="text-registerMesh">
+                        <h1 className="text-registerMesh-main"></h1>
+                    </div>
+                </div>
+                <div className="enterWire">
+                    <div className="icon-enterWire">
+                        <img src="s" alt="" className="icon-enterWire-main" />
+                    </div>
+                    <div className="text-enterWire">
+                        <h1 className="text-enterWire-main"></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+}

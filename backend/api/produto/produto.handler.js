132dd6135f92c6fc46b422151298e64ae917f9be
Thiago <106108469+Thiago-M-Braga@users.noreply.github.com>
2022-08-10 21:22:05 -0300

    21:21 - 10/08
    Algumas verificações foram feitas

--- backend/api/produto/produto.handler.js
+++ backend/api/produto/produto.handler.js
@@ -1,17 +1,26 @@
 const crud = require("../../CRUD/server");
-const tabela = "produto"
+const tabela = "produto";
+const listaProduto = await crud.buscar(tabela);
 
 async function procurarProdutos() {
      return await crud.buscar(tabela);
 }
 
 async function procurarProduto(id) {
-    return await crud.buscarPorId(tabela, id);
+    if (listaProduto.filter((Produto) => Produto.id == id).length == 0) {
+        return await "Este ID não foi encontrado!"
+    } else {
+        return await crud.buscarPorId(tabela, id);
+    }
 }
 
 async function criarProduto(dados) {
     if (dados.descricao && dados.quantidade && dados.valorTotal && dados.arquivoNF && dados.idFornecedor) {
-        return await crud.salvar(tabela, false, dados);
+        if (listaProduto.filter((Produto) => Produto.descricao == dados.descricao && Produto.idFornecedor == dados.idFornecedor).length == 0) {
+            return await crud.salvar(tabela, false, dados);
+        } else {
+            return await "Erro! Já existe este produto!"
+        }
     } else {
         return await "Erro! Falta algum dado!"
     }
@@ -19,14 +28,22 @@ async function criarProduto(dados) {
 
 async function editarProduto(dados, id){
     if (dados.id && dados.descricao && dados.quantidade && dados.valorTotal && dados.arquivoNF && dados.idFornecedor) {
-        return await crud.salvar(tabela, id, dados);
+        if (listaProduto.filter((Produto) => Produto.descricao == dados.descricao && Produto.idFornecedor == dados.idFornecedor).length == 0) {
+            return await crud.salvar(tabela, id, dados);
+        } else {
+            return await "Erro! Já existe este produto!"
+        }
     } else {
         return await "Erro! Falta algum dado!"
     }
 }
 
 async function deletarProduto(id){
-    return await crud.remover(tabela, id);
+    if (listaProduto.filter((Produto) => Produto.id == id).length == 0) {
+        return await "Este ID não foi encontrado!"
+    } else {
+        return await crud.remover(tabela, id);
+    }
 }
 
 

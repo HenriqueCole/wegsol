c5d059e589499272e93139e1eeaff5e7900686c9
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-08-10 21:06:10 -0300

    restauração do tamanho do container

--- frontend/system-front/src/clientRegister/index.jsx
+++ frontend/system-front/src/clientRegister/index.jsx
@@ -4,31 +4,34 @@ import "./styles.scss";
 
 export default function clientRegister() {
   return (
-    <div className="containerPageClientRegister">
-      <main>
-        <div className="containerModalRegister">
-          <div className="headerModal">
-            <span>Cadastrar cliente</span>
+
+    <div className="container">
+      <div className="containerPageClientRegister">
+        <main>
+          <div className="containerModalRegister">
+            <div className="headerModal">
+              <span>Cadastrar cliente</span>
+            </div>
+            <div className="containerInputs">
+
+
+              <label>
+                <span>Nome:</span>
+                <input type="text" />
+              </label>
+
+
+              <label>
+                <span>CNPJ:</span>
+                <input type="text" />
+              </label>
+            </div>
+            <div className="containerButton">
+              <button>Concluir</button>
+            </div>
           </div>
-          <div className="containerInputs">
-
-
-            <label>
-              <span>Nome:</span>
-              <input type="text" />
-            </label>
-
-
-            <label>
-              <span>CNPJ:</span>
-              <input type="text" />
-            </label>
-          </div>
-          <div className="containerButton">
-            <button>Concluir</button>
-          </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   );
 }

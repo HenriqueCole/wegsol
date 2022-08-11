5296816191908cf9045792007fad7b9cb23a24b3
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-08-10 21:39:04 -0300

    sidebar

--- frontend/system-front/src/index.jsx
+++ frontend/system-front/src/index.jsx
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./Home";
+import Sidebar from "./Components/sidebar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+       
+        <Route path="/sidebar" element={<Sidebar />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

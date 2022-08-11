77eda5a0a556e6b725406fa6ab40de96ef554797
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-08-10 21:22:10 -0300

    a

--- frontend/system-front/src/machineRegister/index.jsx
+++ frontend/system-front/src/machineRegister/index.jsx
@@ -19,7 +19,63 @@ export default function machineRegister() {
               <span>Marca:</span>
               <input type="text" />
             </label>
+
+
+            <div className="flex">
+              <label>
+                <span>Ano fabricação:</span>
+                <input type="text" />
+              </label>
+
+              <label>
+                <span>Ano compra:</span>
+                <input type="text" />
+              </label>
+            </div>
+
+            <div className="flex">
+
+              <label>
+                <span>Valor compra:</span>
+                <input type="text" />
+              </label>
+
+              <label>
+                <span>RPM:</span>
+                <input type="text" />
+              </label>
+            </div>
           </div>
+
+          <p>Quantidades</p>
+
+          <div className="flex">
+            <label>
+              <span>RPM:</span>
+              <input type="text" />
+            </label>
+
+            <label>
+              <span>RPM:</span>
+              <input type="text" />
+            </label>
+
+            <label>
+              <span>RPM:</span>
+              <input type="text" />
+            </label>
+
+            <label>
+              <span>RPM:</span>
+              <input type="text" />
+            </label>
+
+            <label>
+              <span>RPM:</span>
+              <input type="text" />
+            </label>
+          </div>
+
           <div className="containerButton">
             <button>Concluir</button>
           </div>

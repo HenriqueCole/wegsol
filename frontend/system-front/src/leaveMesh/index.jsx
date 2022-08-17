128e967ebfe43ed08e8ee577019716154d90862e
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-08-17 18:47:12 -0300

    changes

---
+++ frontend/system-front/src/leaveMesh/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "../styles.scss";
+import Sidebar from "../Components/sidebar"
+
+export default function leaveMesh() {
+    return (
+        <div className="container">
+            <Sidebar></Sidebar>
+            <main>
+                <div className="form">
+                    <div className="headerModal">
+                        <span>Saída de Malha</span>
+                    </div>
+                    <label>
+                        <span>Adicionar saídas:</span>
+                        <input type="text"></input>
+                    </label>
+
+                    <label className="malhas">
+
+                        <div className="malha">
+                            <input type="text" disabled placeholder="Malha 01"></input>
+                            <img src="https://i.imgur.com/05V0IrH.png" alt="" />
+                        </div>
+                    </label>
+
+                    <label>
+                        <span>Anexo NF:</span>
+                        <input type="file" />
+                    </label>
+
+                    <label>
+                        <span>Fornecedor:</span>
+                        <select>
+                            <option>caraio</option>
+                        </select>
+                    </label>
+
+                    <div className="containerButton">
+                        <button>Concluir</button>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
--- frontend/system-front/src/wireRegister/index.jsx
+++ frontend/system-front/src/wireRegister/index.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import "../styles.scss";
 import Sidebar from "../Components/sidebar"
 
-export default function providerRegister() {
+export default function wireRegister() {
     return (
         <div className="container">
             <Sidebar></Sidebar>
             <main>
                 <div className="form">
                     <div className="headerModal">
-                        <span>Cadastrar Produto</span>
+                        <span>Cadastrar Fio</span>
                     </div>
                     <label>
                         <span>Descrição:</span>
                         <textarea cols="30" rows="10"></textarea>
                     </label>
+
+                    <div className="containerButton">
+                        <button>Concluir</button>
+                    </div>
                 </div>
             </main>
         </div>

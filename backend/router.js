2c241383b79d48e65874c49ac112fe8bfc81cfe0
Thiago-M-Braga <106108469+Thiago-M-Braga@users.noreply.github.com>
2022-08-17 19:43:01 -0300

    Update router.js

--- backend/router.js
+++ backend/router.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-/*Examples
-const clientes = require("./api//clientes/clientes.controller");
-router.use("/clientes", clientes);
-*/
-
 const fornecedor = require("./api/fornecedor/fornecedor.controller");
 const fio = require("./api/fio/fio.controller");
 const entrada_de_fio = require("./api/entrada_de_fio/entrada_de_fio.controller");
@@ -37,5 +32,7 @@ router.use("/funcionario", funcionario);
 router.use("/maquina", maquina);
 router.use("/producao", producao);
 router.use("/malha_do_cliente", malha_do_cliente);
+
+
 
 module.exports = router;

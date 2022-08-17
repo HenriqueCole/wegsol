4a98e301300f1e50902146b56d27a17ab1fbba70
HenriqueCole <henriquecolefernandes@gmail.com>
2022-08-17 19:17:03 -0300

    increase value

--- frontend/system-front/src/productRegister/index.jsx
+++ frontend/system-front/src/productRegister/index.jsx
@@ -1,54 +1,70 @@
 import React from "react";
 import "../styles.scss";
-import Sidebar from "../Components/sidebar"
+import Sidebar from "../Components/sidebar";
 
 export default function productRegister() {
-    return (
-        <div className="container">
-            <Sidebar></Sidebar>
-            <main>
-                <div className="form">
-                    <div className="headerModal">
-                        <span>Cadastrar Produto</span>
-                    </div>
-                    <div className="containerInputs">
-                        <label>
-                            <span>Descrição:</span>
-                            <textarea cols="30" rows="10"></textarea>
-                        </label>
+  function increaseValue(e) {
+    let input = document.getElementById("quantidade");
+    input.value = parseInt(input.value) + 1;
+  }
 
-                        <label>
-                            <span>Quantidade:</span>
-                            <div className="quantidade">
-                                <div className="menos">-</div>
-                                <input type="text" value={0} />
-                                <div className="mais">+</div>
-                            </div>
-                        </label>
+  function decreaseValue(e) {
+    let input = document.getElementById("quantidade");
+    input.value = parseInt(input.value) - 1;
+    if (input.value < 0) {
+      input.value = 0;
+    }
+  }
 
-                        <label>
-                            <span>Valor Total:</span>
-                            <input type="text" />
-                        </label>
+  return (
+    <div className="container">
+      <Sidebar></Sidebar>
+      <main>
+        <div className="form">
+          <div className="headerModal">
+            <span>Cadastrar Produto</span>
+          </div>
+          <div className="containerInputs">
+            <label>
+              <span>Descrição:</span>
+              <textarea cols="30" rows="10"></textarea>
+            </label>
 
-                        <label>
-                            <span>Anexo NF:</span>
-                            <input type="file" />
-                        </label>
-
-                        <label>
-                            <span>Fornecedor:</span>
-                            <select>
-                                <option>caraio</option>
-                            </select>
-                        </label>
-
-                    </div>
-                    <div className="containerButton">
-                        <button>Concluir</button>
-                    </div>
+            <label>
+              <span>Quantidade:</span>
+              <div className="quantidade">
+                <div className="menos" onClick={(e) => decreaseValue(e)}>
+                  -
                 </div>
-            </main>
+                <input id="quantidade" type="text" value={0} />
+                <div className="mais" onClick={(e) => increaseValue(e)}>
+                  +
+                </div>
+              </div>
+            </label>
+
+            <label>
+              <span>Valor Total:</span>
+              <input type="text" />
+            </label>
+
+            <label>
+              <span>Anexo NF:</span>
+              <input type="file" />
+            </label>
+
+            <label>
+              <span>Fornecedor:</span>
+              <select>
+                <option>caraio</option>
+              </select>
+            </label>
+          </div>
+          <div className="containerButton">
+            <button>Concluir</button>
+          </div>
         </div>
-    );
+      </main>
+    </div>
+  );
 }

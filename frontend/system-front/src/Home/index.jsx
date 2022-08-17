ee98aadd6052a83f02be2384537d81120225f6d5
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-08-17 18:31:05 -0300

    fixed

--- frontend/system-front/src/Home/index.jsx
+++ frontend/system-front/src/Home/index.jsx
@@ -7,11 +7,14 @@ export default function App() {
   return (
     <div className="containerPageHome">
       <Sidebar></Sidebar>
-      <div className="quadrado-cima"></div>
 
-      <div className="quadrados">
-        <div className="quadrado-esquerda"></div>
-        <div className="quadrado-direita"></div>
+      <div class="container">
+        <div className="quadrado-cima"></div>
+
+        <div className="quadrados">
+          <div className="quadrado-esquerda"></div>
+          <div className="quadrado-direita"></div>
+        </div>
       </div>
     </div>
   );

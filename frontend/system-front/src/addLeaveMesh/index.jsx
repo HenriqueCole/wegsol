31a0c4ba5a7f4e4555045dc61141615fe846d066
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-08-31 20:45:05 -0300

    a

--- frontend/system-front/src/addLeaveMesh/index.jsx
+++ frontend/system-front/src/addLeaveMesh/index.jsx
@@ -11,18 +11,80 @@ export default function addLeaveMesh() {
             <main>
                 <div className="form">
                     <div className="headerModal">
-                        <span>Cadastrar Fio</span>
+                        <span>Adicionar Saída</span>
                     </div>
 
-                    <label>
-                        <span>Quantidade:</span>
-                        <div className="quantidade">
-                            <div className="menos">-</div>
-                            <input type="text" value={0} />
-                            <div className="mais">+</div>
-                        </div>
-                    </label>
 
+                    <div className="topContainer">
+                        <label>
+                            <span>Quantidade:</span>
+                            <div className="quantidade">
+                                <div className="menos">-</div>
+                                <input type="text" value={0} />
+                                <div className="mais">+</div>
+                            </div>
+                        </label>
+
+                        <label className="labelPeso">
+                            <span>Peso:</span>
+                            <div className="inputPeso">
+                                <input type="text" />
+                                <span>     Kg</span>
+                            </div>
+                        </label>
+                    </div>
+
+                    <br />
+
+                    <div className="qualityContainer">
+                        <label className="labelQuality">
+                            <label className="titleAndInput">
+                                <span>Qualidade</span>
+                                <div className="inputQualidade">
+                                    <input type="text" className="inputQuality" />
+                                </div>
+                            </label>
+                        </label>
+                    </div>
+
+                    <br />
+
+                    <div className="nfContainer">
+                        <label>
+                            <span>Anexar Nota Fiscal: </span>
+                            <div className="buttonUpload">
+                                <input type="file" />
+                            </div>
+                        </label>
+                    </div>
+
+                    <br />
+
+                    <div className="priceContainer">
+                        <label className="labelPeso">
+                            <span>Preço:</span>
+                            <div className="inputPeso">
+                                <input type="text" />
+                                <span>   R$</span>
+                            </div>
+                        </label>
+                    </div>
+
+                    <br />
+
+                    <div className="priceContainer">
+                        <label className="labelPeso">
+                            <span>Malha: </span>
+                            <div className="inputPeso">
+                                <input list="mesh" name="mesh" id="meshInput" />
+                                <datalist id="mesh">
+                                    <option value="Poliêster" />
+                                    <option value="Algodão" />
+                                    <option value="DryFit" />
+                                </datalist>
+                            </div>
+                        </label>
+                    </div>
 
                     <div className="containerButton">
                         <button>Concluir</button>

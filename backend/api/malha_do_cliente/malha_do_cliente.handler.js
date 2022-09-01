2f3587d59cf7f07e2b1f9e3925957627c3c04481
Kenzohfs <88584837+Kenzohfs@users.noreply.github.com>
2022-08-31 21:22:20 -0300

    arrumado e tirado a verificação para cadastrar uma malha do cliente

--- backend/api/malha_do_cliente/malha_do_cliente.handler.js
+++ backend/api/malha_do_cliente/malha_do_cliente.handler.js
@@ -1,45 +1,38 @@
 const crud = require('../../crud/server');
+const tabela = "malha_do_cliente"
 
-async function criarMalha_Do_Cliente(dadosReq){
+async function criarMalha_Do_Cliente(dadosReq) {
     const malhaCliente = dadosReq;
-    const clientes = await crud.buscar("cliente");
-    const malhas = await crud.buscar("malha");
-    
-    if(malhas.findIndex(m => m.id == malhaCliente.idCliente) == -1){
-        return "Erro! Id do cliente não encontrado.";
-    }else if(clientes.findIndex(c => c.id == malhaCliente.idMalha) == -1){
-        return "Erro! Id da malha não encontrado.";
-    }else{
-        const dados = await crud.salvar("malha", null, req.body);
-        return dados;
-    }
+
+    const dados = await crud.salvar(tabela, null, malhaCliente);
+    return dados;
 }
 
-async function procurarMalhas_Do_Cliente(){
-    const dados = await crud.buscar("malha");
+async function procurarMalhas_Do_Cliente() {
+    const dados = await crud.buscar(tabela);
     return (dados);
 }
 
-async function procurarMalha_Do_Cliente(id){
-    const dados = await crud.buscarPorId("malha", id);
+async function procurarMalha_Do_Cliente(id) {
+    const dados = await crud.buscarPorId(tabela, id);
     return dados;
 }
 
-async function deletarMalha_Do_Cliente(req, res){
-    const malha = await crud.buscar("malha"); 
-    if(malha.findIndex(c => c.idMALHA == req.params.idMALHA) != -1){
-    const dados = await crud.remover("cliente", req.params.id);
-    return dados;
-    }else{
+async function deletarMalha_Do_Cliente(req, res) {
+    const malha = await crud.buscar("malha");
+    if (malha.findIndex(c => c.idMALHA == req.params.idMALHA) != -1) {
+        const dados = await crud.remover("cliente", req.params.id);
+        return dados;
+    } else {
         res.status(404).send("id inválido");
     }
 }
 
-async function editarMalha_Do_Cliente(req, res){
+async function editarMalha_Do_Cliente(req, res) {
     const malha = await crud.buscar("malha");
-    if(malha.findIndex(c => c.idMALHA == req.params.idMALHA)!= -1){
+    if (malha.findIndex(c => c.idMALHA == req.params.idMALHA) != -1) {
         return await crud.deletar("cliente", req.params.id);
-    }else{
+    } else {
         res.status(404).send("id inválido");
     }
 }

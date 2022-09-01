26dc08a6d1c3c72f1983a11bfad2d58fad8a70d8
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-08-31 21:47:26 -0300

    fix header

--- frontend/system-front/src/Components/Header/header.jsx
+++ frontend/system-front/src/Components/Header/header.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 import Logo from "../../assets/logo.png";
 import "./header.scss";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return(
+    return (
         <header>
-            <div className="logo">
-                <img src={Logo} alt="logo" className="logo-img" />
-            </div>
-            <div className="name-title">
-                <h1>Wegsol</h1>
-            </div>
+            <Link to={'/'}>
+                <div className="logo">
+                    <img src={Logo} alt="logo" className="logo-img" />
+                </div>
+            </Link>
+          
         </header>
     )
 }

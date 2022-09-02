c6f93da7a4d2f368d2752c9bcca6f30498e93c7b
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-09-02 19:42:55 -0300

    create service and cors

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const port = 8080;
+const cors = require("cors");
+
+
 
 const router = require("./router");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 
 app.use("/api", router);
 
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
 })
-
-// module.exports = app;

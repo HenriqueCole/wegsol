d43d4ad4ab00f2938e3dbf0c334ad4570d1079d9
Vytor.js <92130250+vytorJS@users.noreply.github.com>
2022-09-02 21:02:18 -0300

    :frog:

--- frontend/system-front/src/services/service.ts
+++ frontend/system-front/src/services/service.ts
@@ -43,6 +43,24 @@ const Services = {
         })
     },
 
+    cadastrarEntradaFio: function (qtd_caixa, qtd_kg, subtotal, qtd_rolos_por_caixa, arquivo_nf, idFornecedor, idFio, idCliente) {
+        return new Promise((resolve, reject) => {
+            fetch('http://127.0.0.1:8080/api/entrada_de_fio', { method: 'POST', body: JSON.stringify({ qtd_caixa: qtd_caixa, subtotal: subtotal, qtd_kg: qtd_kg, qtd_rolos_por_caixa: qtd_rolos_por_caixa, arquivo_nf: arquivo_nf, idFornecedor: idFornecedor, idFio: idFio, idCliente: idCliente }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
+    cadastrarSaidaMalha: function (qtd_rolos, peso, qualidade, valor_saida, arquivo_nf, idMalha, idSaidas_De_Malhas) {
+        return new Promise((resolve, reject) => {
+            fetch('http://127.0.0.1:8080/api/saida_de_malha', { method: 'POST', body: JSON.stringify({ qtd_rolos: qtd_rolos, peso: peso, qualidade: qualidade, valor_saida: valor_saida, arquivo_nf: arquivo_nf, idMalha: idMalha, idSaidas_De_Malhas: idSaidas_De_Malhas }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
+    cadastrarProducao: function (kg_rolo_malha, defeito, idCliente, idMaquina, idFuncionario) {
+        return new Promise((resolve, reject) => {
+            fetch('http://127.0.0.1:8080/api/funcionario', { method: 'POST', body: JSON.stringify({ kg_rolo_malha: kg_rolo_malha, defeito: defeito, idCliente: idCliente, idMaquina: idMaquina, idFuncionario: idFuncionario }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    }
+
 }
 
 

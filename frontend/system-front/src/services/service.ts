2d1f1d273f78f1178e5be9cb7e3d7a44ac8cc900
Vytor.js <92130250+vytorJS@users.noreply.github.com>
2022-09-14 19:44:26 -0300

    :frog:

--- frontend/system-front/src/services/service.ts
+++ frontend/system-front/src/services/service.ts
@@ -11,9 +11,21 @@ const Services = {
         })
     },
 
+    buscarClientes: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/cliente', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
     cadastrarMaquina: function (nome, marca, anoFabicacao, anoCompra, valorCompra, RPM, agulhas, platinas, blocos, gaiolas, cones, idMalha) {
         return new Promise((resolve, reject) => {
             fetch(url + '/maquina', { method: 'POST', body: JSON.stringify({ nome: nome, marca: marca, ano_fabricacao: anoFabicacao, ano_compra: anoCompra, valor_compra: valorCompra, rpm: RPM, qtd_agulhas: agulhas, qtd_blocos: blocos, qtd_platinas: platinas, qtd_gaiolas: gaiolas, qtd_cones_por_gaiola: cones, idMalha: idMalha }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
+    buscarMaquinas: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/maquina', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
         })
     },
 
@@ -23,9 +35,21 @@ const Services = {
         })
     },
 
+    buscarProdutos: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/produtos', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
     cadastrarFornecedor: function (nome, cnpj) {
         return new Promise((resolve, reject) => {
             fetch(url + '/fornecedor', { method: 'POST', body: JSON.stringify({ nome: nome, cnpj: cnpj }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
+    buscarFornecedores: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/fornecedor', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
         })
     },
 
@@ -35,9 +59,21 @@ const Services = {
         })
     },
 
+    buscarMalha: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/malha', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
     cadastrarFio: function (descricao) {
         return new Promise((resolve, reject) => {
             fetch(url + '/fio', { method: 'POST', body: JSON.stringify({ descricao: descricao }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
+    buscarFios: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/fios', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
         })
     },
 
@@ -47,9 +83,21 @@ const Services = {
         })
     },
 
+    buscarFuncionarios: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/funcionario', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
     cadastrarEntradaFio: function (qtd_caixa, qtd_kg, subtotal, qtd_rolos_por_caixa, arquivo_nf, idFornecedor, idFio, idCliente) {
         return new Promise((resolve, reject) => {
             fetch(url + '/entrada_de_fio', { method: 'POST', body: JSON.stringify({ qtd_caixa: qtd_caixa, subtotal: subtotal, qtd_kg: qtd_kg, qtd_rolos_por_caixa: qtd_rolos_por_caixa, arquivo_nf: arquivo_nf, idFornecedor: idFornecedor, idFio: idFio, idCliente: idCliente }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
+    buscarEntradaFio: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/entrada_de_fio', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
         })
     },
 
@@ -59,9 +107,21 @@ const Services = {
         })
     },
 
+    buscarSaidaMalha: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/saida_de_malha', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
     cadastrarProducao: function (kg_rolo_malha, defeito, idCliente, idMaquina, idFuncionario) {
         return new Promise((resolve, reject) => {
-            fetch(url + '/funcionario', { method: 'POST', body: JSON.stringify({ kg_rolo_malha: kg_rolo_malha, defeito: defeito, idCliente: idCliente, idMaquina: idMaquina, idFuncionario: idFuncionario }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+            fetch(url + '/producao', { method: 'POST', body: JSON.stringify({ kg_rolo_malha: kg_rolo_malha, defeito: defeito, idCliente: idCliente, idMaquina: idMaquina, idFuncionario: idFuncionario }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
+        })
+    },
+
+    buscarProducoes: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url + '/producao', { method: 'GET' }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
         })
     }
 
